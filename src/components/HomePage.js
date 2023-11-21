b48eb3c60f60d1e99ff18a53525c018a24c789f0
Elkadhi Oussama <83676401+elkadhi-oussama@users.noreply.github.com>
2023-11-21 21:28:02 +0100

    add Timer

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -7,6 +7,7 @@ const HomePage = () => {
         <img src="./images/download.png" alt="" />
         <h2> Welcome to Go My Code Quiz </h2>
         <p> This Quiz for test your skills to learn the second level in web development </p>
+        <span>You have only 20 minutes to answer</span>
         <span> Best Of Luck</span>
         <Link to="/quiz"> <button>Start</button></Link>
     </div>
