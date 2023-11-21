043e17346e0c3550d4352e1180a11b875959fc02
Elkadhi Oussama <83676401+elkadhi-oussama@users.noreply.github.com>
2023-11-21 21:19:48 +0100

    add Timer

--- src/components/ListQuestion.js
+++ src/components/ListQuestion.js
@@ -1,14 +1,48 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Quiz from "./Quiz";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const ListQuestion = ({ data, setCountRepCorrect, countRepCorrect }) => {
   // this state for save id of answer user
   const [forSaveID, setforSaveID] = useState([]);
   //end
+  // timer
+  const [timeLeft, setTimeLeft] = useState(1 * 60);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (timeLeft > 0) {
+        setTimeLeft(timeLeft - 1);
+      }
+    }, 1000);
+
+    return () => clearInterval(interval); // Cleanup the interval on unmount
+  }, [timeLeft]);
+
+  //end Timer
+  //Push User to Result Comp
+  const navigate = useNavigate();
+  if (timeLeft === 0) {
+    navigate("/result")
+  }
+  //end
+
   return (
     <>
-    <h1 className='tit'>Quiz App Introduction to Web Development</h1>
+      <p className="timer">
+        Time left:{" "}
+        <span
+          className={
+            timeLeft < 60 && timeLeft > 30
+              ? "lessThenTen"
+              : timeLeft <= 30
+              ? "lessThenFive"
+              : ""
+          }
+        >
+          {parseInt(timeLeft / 60)}:{timeLeft % 60}
+        </span>
+      </p>
+      <h1 className="tit">Quiz App Introduction to Web Development</h1>
       <div className="listQuiz">
         {data.map((item) => (
           <Quiz
@@ -21,7 +55,9 @@ const ListQuestion = ({ data, setCountRepCorrect, countRepCorrect }) => {
           />
         ))}
       </div>
-      <Link to='/result' ><button className="BtnSubmit" >Submit</button></Link>
+      <Link to="/result">
+        <button className="BtnSubmit">Submit</button>
+      </Link>
     </>
   );
 };
